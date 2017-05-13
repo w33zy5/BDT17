c3d635241cb69a87e476823f9d241015011da6d0
I Dewa Gede Agung Krisnayana <dewaagung44@gmail.com>
2017-05-13 15:19:08 +0800

    penambahan console dan beberapa fungsi

--- calculator.js
+++ calculator.js
@@ -1,20 +1,32 @@
 var calculator = {
 	
 	tambah : function(a,b){
-		return
+		return a+b;
 	},
-	kurang : '',
-	kali : '',
+	kurang : function(a,b){
+		return a-b;
+	},
+	kali : function(a,b){
+		return a*b;
+	},
+	bagi : function(a,b){
+		return a/b;
+	}
 	samadengan : function(a,b){
 	//jika lebih besar a
 		if(a>b){
-			return a + 'lebih besar dari ' + b;
+			return a + ' lebih besar dari ' + b;
 		}
 		else if(b<a){
-			return b + 'lebih besar dari ' + a;
+			return b + ' lebih besar dari ' + a;
 		}
-		else{
-			return 'Test';
+		else if(a==b){
+			return a + ' sama dengan ' + b;
 		}
 	}
 }
+
+console.log(calculator.tambah(1,2));
+console.log(calculator.kurang(2,3));
+console.log(calculator.kali(4,5));
+console.log(calculator.bagi(6,7));
